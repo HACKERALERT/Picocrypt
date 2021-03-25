4b66acc908dcdba395469a57f095cdcfc26b210f
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2021-03-25 18:06:12 -0400

    Delete cm.c

--- src/unstable/cm.c
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <Windows.h>
-
-int main(int argc,char *argv[]){
-	::ShowWindow(::GetConsoleWindow(), SW_SHOW);
-	std::system((std::string("cd picocrypt && start.vbs \"")+argv[1]+"\"").c_str());
-}
