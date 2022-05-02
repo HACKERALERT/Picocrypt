bb3206576b8f1a6787e32c3c742be7273bad3759
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2022-05-02 18:05:54 -0400

    Begin v1.28

--- src/Picocrypt.go
+++ src/Picocrypt.go
@@ -2,7 +2,7 @@ package main
 
 /*
 
-Picocrypt v1.27
+Picocrypt v1.28
 Copyright (c) Evan Su (https://evansu.cc)
 Released under a GNU GPL v3 License
 https://github.com/HACKERALERT/Picocrypt
@@ -48,7 +48,7 @@ import (
 )
 
 // Generic variables
-var version = "v1.27"
+var version = "v1.28"
 var window *giu.MasterWindow
 var dpi float32
 var mode string
