6ac05ef69db23c62e476876a94b08ca6c9386adc
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2023-04-12 23:11:20 -0400

    Bump version

--- src/Picocrypt.go
+++ src/Picocrypt.go
@@ -2,7 +2,7 @@ package main
 
 /*
 
-Picocrypt v1.31
+Picocrypt v1.32
 Copyright (c) Evan Su
 Released under a GNU GPL v3 License
 https://github.com/HACKERALERT/Picocrypt
@@ -58,7 +58,7 @@ var TRANSPARENT = color.RGBA{0x00, 0x00, 0x00, 0x00}
 
 // Generic variables
 var window *giu.MasterWindow
-var version = "v1.31"
+var version = "v1.32"
 var dpi float32
 var mode string
 var working bool
